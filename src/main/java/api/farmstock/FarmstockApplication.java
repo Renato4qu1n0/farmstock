546e5d21115f4da2c167a695364672051cec79be
Renato Aquino <re4qu1n0@gmail.com>
2024-12-17 20:58:58 -0300

    Primeiro Commit da FarmStock

---
+++ src/main/java/api/farmstock/FarmstockApplication.java
@@ -0,0 +1,13 @@
+package api.farmstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/farmstock/FarmstockApplicationTests.java
@@ -0,0 +1,13 @@
+package api.farmstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
